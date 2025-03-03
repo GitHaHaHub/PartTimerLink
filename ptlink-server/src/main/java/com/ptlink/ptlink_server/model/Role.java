760ecbad96fcbb8bd1e99e2de5a328ceed84afbd
haha <swidhaha@outlook.com>
2025-03-04 01:07:02 +0900

    feat: add DB connection
    Resolves #PTL-10

---
+++ ptlink-server/src/main/java/com/ptlink/ptlink_server/model/Role.java
@@ -0,0 +1,25 @@
+package com.ptlink.ptlink_server.model;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Getter
+@Setter
+@Entity
+@AllArgsConstructor
+@NoArgsConstructor
+public class Role {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Integer roleId;
+
+    private String roleName;
+
+    private String permissions;
+}
