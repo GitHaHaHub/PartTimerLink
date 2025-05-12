ad9598dd4c715103110e13ab2862477de5a41bc8
GitHaHaHub <92131005+GitHaHaHub@users.noreply.github.com>
2025-05-12 23:01:14 +0900

    feat: add homepage with signin
    build: apply react-router data mode
    
    build: seperate backend api url into env var and update github actions workflow
    
    Related to #PTL-22

--- ptlink-client/src/App.tsx
+++ ptlink-client/src/App.tsx
@@ -1,14 +1,80 @@
 import './App.css'
-import { redirect } from 'react-router'
+import React, { useState } from 'react';
+import axios from 'axios';
+import { useNavigate } from 'react-router'
+import api from './api';
 
 function App() {
+  const [username, setUsername] = useState('');
+  const [password, setPassword] = useState('');
+  const navigate = useNavigate();
+
+  const handleSubmit = (event: React.FormEvent) => {
+    event.preventDefault();
+
+    console.log('Username:', username);
+    console.log('Password:', password);
+
+    // On successful login, redirect to the home page
+    // Add your authentication logic here
+    api.post('/generateToken', {
+      username: username,
+      password: password,
+    },
+    { withCredentials: true }
+    ).then((response) => {
+      console.log(response.data);
+      axios.defaults.headers.common['Authorization'] = `${response.data.Authorization}`;
+      console.log(axios.defaults.headers.common['Authorization']);
+      navigate('/');
+    }
+    ).catch((error) => {
+      console.error(error);
+      alert('Invalid credentials');
+    }
+    );
+  };
 
   return (
-    <div>
-      <div className="App">Home</div>
-      <button type="button" onClick={() => redirect('/mypage')}>Sign Up</button>
+    <div className="login-wrapper">
+      <h1 className="title">PartTimerLink</h1>
+      <p className="subtitle">Your Part-time Job Partner</p>
+
+      <div className="login-container">
+        <form onSubmit={handleSubmit}>
+          <div className="form-group">
+            <label htmlFor="username">Username</label>
+            <input
+              type="text"
+              id="username"
+              value={username}
+              onChange={(e) => setUsername(e.target.value)}
+              placeholder="johndoe123"
+              required
+            />
+          </div>
+
+          <div className="form-group">
+            <label htmlFor="password">Password</label>
+            <input
+              type="password"
+              id="password"
+              value={password}
+              onChange={(e) => setPassword(e.target.value)}
+              placeholder="********"
+              required
+            />
+          </div>
+
+          <button type="submit" className="sign-in-btn">Sign In</button>
+        </form>
+
+        <button type="button" className="sign-up-link" onClick={() => navigate('/signup')}>
+          Don’t have an account?
+        </button>
+      </div>
     </div>
-  )
-}
+  );
+};
 
 export default App
